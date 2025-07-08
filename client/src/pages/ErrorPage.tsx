2903f1b7da85898ffc5b8cf70667225574ffd15a
THEOPHILE PICOLET <theophilepicolet@hotmail.fr>
2025-07-08 12:49:11 +0200

    initial app front and github pages done config

---
+++ client/src/pages/ErrorPage.tsx
@@ -0,0 +1,9 @@
+export default function ErrorPage() {
+  return (
+    <>
+      <section className="erreur404">
+        <p>Erreur 404</p>
+      </section>
+    </>
+  );
+}
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Simplon_app_front",
   server: {
     port: 3000,
   },
